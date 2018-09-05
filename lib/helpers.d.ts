275098550826deb15f5acc6ef5fb3b89114d3679
Bart Langelaan <langelaanbart@gmail.com>
2018-09-05 11:16:15 +0200

    build: remove lib folder from git, but keep it on npm

--- lib/helpers.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function flattenTranslations(translations: Translations[]): Translations;
-export declare function isObject(o: any): boolean;
-export declare function isPluralized(translations: Translations): boolean;
-export declare function determinePluralizedTranslationKey(value: number): PluralizeKey;
--- lib/index.d.ts
+++
@@ -1,1 +0,0 @@
-export default function init(translations: TranslationInput, globalOptions: GlobalOptions): (lookupKey: string, translationOptions?: TranslationOptions | undefined) => string;
